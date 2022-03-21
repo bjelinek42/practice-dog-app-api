47108491e9dc00b7bbecd5bfbe1b718224f2492d
ben <bjelinek42@gmail.com>
2022-03-21 15:44:04 -0600

    index controller and seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+dog = Dog.new(name: "Howie", age: 4, breed: "Bichon Frise")
+dog.save
+
+dog = Dog.new(name: "Aspen", age: 8, breed: "Pitbull")
+dog.save
+
+dog = Dog.new(name: "Django", age: 5, breed: "Blue Pitbull")
+dog.save
+
+dog = Dog.new(name: "Rosie", age: 9, breed: "Re Pitbull")
+dog.save
